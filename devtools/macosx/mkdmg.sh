5c472636586dd3f865f67d8fa25ed61e62fd8ee7
Sergey Prokhorchuk <s.stager@gmail.com>
2012-08-20 22:27:40 +0300

    icon.icns MacOS X icon added (based on ngplant.svg)

--- devtools/macosx/mkdmg.sh
+++ devtools/macosx/mkdmg.sh
@@ -3,11 +3,14 @@
 IMAGE_DIR=image
 CONTENTS_DIR=$IMAGE_DIR/ngplant.app/Contents
 CONTENTS_MACOS_DIR=$CONTENTS_DIR/MacOS
+CONTENTS_RESOURCES_DIR=$CONTENTS_DIR/Resources
 
 mkdir -p   $CONTENTS_DIR
 mkdir -p   $CONTENTS_MACOS_DIR
+mkdir -p   $CONTENTS_RESOURCES_DIR
 cp ngplant $CONTENTS_MACOS_DIR
 cp Info.plist $CONTENTS_DIR
+cp icon.icns $CONTENTS_RESOURCES_DIR
 pushd $IMAGE_DIR
 ln -s /Applications Applications
 popd
