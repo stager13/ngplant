60373af016ceca94bb01c8132d86d58c0aa20119
Sergey Prokhorchuk <stager@users.sourceforge.net>
2012-11-25 21:32:09 +0200

    version has been set to 0.9.9

--- ngplant/main.cpp
+++ ngplant/main.cpp
@@ -57,7 +57,7 @@
 
 #include <p3dapp.h>
 
-#define NGPLANT_BASE_VER "0.9.8"
+#define NGPLANT_BASE_VER "0.9.9"
 
 #if defined(EXTRA_VERSION)
  #define NGPLANT_VERSION_STRING NGPLANT_BASE_VER "(" EXTRA_VERSION ")"
